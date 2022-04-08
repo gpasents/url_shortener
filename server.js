476e52165289ca896763c0a32cb3d7002d4097a8
Giorgos Pasentsis <gpasents@gmail.com>
2022-04-08 13:55:46 +0300

    add functionality

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const parser = require('body-parser');
+const dns = require('dns');
+const URL = require('url').URL
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(parser());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +20,31 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+let array = [];
+
+app.post("/api/shorturl",(req,res)=>{
+  let url = req.body.url;
+  let urlObj = new URL(url);
+  if (array.includes(url)){
+    res.json("Url already included at index "+ array.indexOf(url));
+  }else if (url.includes("ftp")){
+    res.json({error: 'invalid url'});
+  }
+  else{
+    array.push(url);
+    console.log(array);
+      res.json({"original_url":url,"short_url":array.indexOf(url)});
+  }
+});
+
+app.get("/api/shorturl/:index",(req,res)=>{
+  let i = req.params.index;
+  if(array[i]){
+    res.redirect(array[i]);
+  }
+  res.json({"error":"url not in memory"});
 });
 
 app.listen(port, function() {
